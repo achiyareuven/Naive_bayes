09c912413356a2bd48aabe5f970a9b267427a546
achiya reuven <achiya3144@gmail.com>
2025-07-22 15:12:47 +0300

    add full pipeline to FastAPI server (load, train, predict)

--- server/server_app.py
+++ server/server_app.py
@@ -3,6 +3,11 @@ from pydantic import BaseModel
 from fastapi import FastAPI,HTTPException
 import uvicorn
 from model.predictor import NaiveBayesPredictor
+from model.Naive_Bayes_Model import NaiveBayesClassifier
+from evaluation.evaluator import Evaluator
+from utils.data_utils import split_train_test,split_feature_target
+from data_cleaner.clean_data import DataCleaner
+from data_loader.load_data_from_csv import CSVLoader
 import dill
 import os
 
@@ -10,31 +15,45 @@ import os
 
 
 app = FastAPI()
-
-model_app = None
+cleaner = DataCleaner()
+load_data_csv  =CSVLoader(r"C:\Users\achiy\PycharmProjects\Naive_bayes\data\phishing.csv")
+model_app =  None
 predictor_app = None
+evaluator = None
 accuracy =None
 
 class PredictionRequest(BaseModel):
     sample: Dict[str, Any]
 
-class LoadModelRequest(BaseModel):
-    path: str
 
 @app.on_event("startup")
-def load_default_model():
+def started():
     global model_app, predictor_app, accuracy
-    model_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "trained_model.pkl")
-    if not os.path.isfile(model_path):
-        print("No default model found. Skipping model load.")
-        return
     try:
-        with open(model_path,"rb") as file:
-            model_app ,accuracy = dill.load(file)
-        predictor_app = NaiveBayesPredictor(model_app)
-    except Exception as e:
-        raise RuntimeError(f"failed to load model on startup: {e}")
+        #load
+        df = load_data_csv.load_data()
 
+        #clean
+        df = cleaner.clean_data(df)
+
+        # split_feature_target
+        X,y =  split_feature_target(df)
+
+        #split_train_test
+        X_train,X_test,y_train,y_test =split_train_test(X,y)
+
+        #training model
+        model_app =NaiveBayesClassifier()
+        model_app.fit(X_train, y_train)
+
+        #prdict
+        predictor_app = NaiveBayesPredictor(model_app)
+
+        # eavalution
+        evaluator = Evaluator(predictor_app)
+        accuracy = evaluator.evaluate_accuracy(X_test, y_test)
+    except Exception as e:
+        raise RuntimeError(f"startup failed: {e}")
 
 
 @app.get("/")
@@ -50,41 +69,23 @@ def get_accuracy():
 @app.post("/predict")
 def predict(request: PredictionRequest):
     if predictor_app is None:
-        raise HTTPException(status_code=500, detail="Model not loaded.")
+            raise HTTPException(status_code=500, detail="Model not loaded.")
     try:
 
-        prediction = predictor_app.predict(request.sample)
-        probabilities = predictor_app.predict_proba(request.sample)
+            prediction = predictor_app.predict(request.sample)
+            probabilities = predictor_app.predict_proba(request.sample)
 
-        prediction_clean = int(prediction) if hasattr(prediction, "item") else prediction
-        probabilities_clean = {
-            str(int(k) if hasattr(k, "item") else k): float(v) if hasattr(v, "item") else v
-            for k, v in probabilities.items()}
+            prediction_clean = int(prediction) if hasattr(prediction, "item") else prediction
+            probabilities_clean = {
+                str(int(k) if hasattr(k, "item") else k): float(v) if hasattr(v, "item") else v
+                for k, v in probabilities.items()}
 
-        return {
-            "prediction": prediction_clean,
-            "probabilities": probabilities_clean
-        }
+            return {
+                "prediction": prediction_clean,
+                "probabilities": probabilities_clean}
+
     except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Prediction failed: {e}")
-
-@app.post("/load_model")
-def load_new_model(request: LoadModelRequest):
-    global model_app, predictor_app, accuracy
-    try:
-        if not request.path.strip():
-            raise HTTPException(status_code=400, detail="Path not provided.")
-        if not os.path.isfile(request.path):
-            raise HTTPException(status_code=400,detail="model file is invalid format ")
-
-        with open(request.path, "rb") as file:
-            model_app, accuracy = dill.load(file)
-        predictor_app = NaiveBayesPredictor(model_app)
-        return {"message": f"Model loaded from {request.path}"}
-    except FileNotFoundError:
-        raise HTTPException(status_code=404, detail="Model file not found.")
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Failed to load model: {e}")
+            raise HTTPException(status_code=400, detail=f"Prediction failed: {e}")
 
 
 if __name__ == "__main__":
