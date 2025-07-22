8481387a545b89a9dadda8692987241a9d353d09
achiya reuven <achiya3144@gmail.com>
2025-07-22 11:40:23 +0300

    I updated the splitting function to be random.

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 
@@ -12,13 +13,23 @@ def split_feature_target(df:pd.DataFrame,target = None):
     return X,y
 
 
-def split_train_test(X,y,size = 0.7):
-    split_index = int(size * len(X))
-    X_train = X.iloc[:split_index]
-    y_train = y.iloc[:split_index]
-    X_test = X.iloc[split_index:]
-    y_test = y.iloc[split_index:]
+# def split_train_test(X,y,size = 0.7):
+#     split_index = int(size * len(X))
+#     X_train = X.iloc[:split_index]
+#     y_train = y.iloc[:split_index]
+#     X_test = X.iloc[split_index:]
+#     y_test = y.iloc[split_index:]
+#     return X_train, X_test, y_train, y_test
+
+
+
+def split_train_test(X, y, size=0.7):
+    stratify = y if len(y.unique()) > 1 else None
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, train_size=size, stratify=stratify, random_state=42
+    )
     return X_train, X_test, y_train, y_test
+
 
 
 
